d4fe2bcb4d196e39b0869ba0e18162bb94ab4052
joshuayeyo <chiikawayeyo@gmail.com>
2025-04-29 07:09:29 +0900

    Init: First commit!

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:carrot_market/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: const Home(),
+    );
+  }
+}

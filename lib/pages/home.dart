79ab2390f6ca12124dfa36198bfc26ad7fd423f1
joshuayeyo <chiikawayeyo@gmail.com>
2025-04-29 12:22:49 +0900

    Feat: Add BottomNavigationBar 구현

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,10 +11,12 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<Map<String, String>> datas = [];
+  late int _currentPageIndex; // late initialization
 
   @override
   void initState() {
     super.initState();
+    _currentPageIndex = 0;
     datas = [
       {
         "image" : "assets/images/ara-1.jpg",
@@ -212,12 +214,44 @@ class _HomeState extends State<Home> {
     );
   }
 
+  BottomNavigationBarItem _bottomNavigationBarItem(String iconName, String label) {
+    return BottomNavigationBarItem(
+        icon: Padding(
+          padding: const EdgeInsets.only(bottom: 3),
+          child: SvgPicture.asset("assets/svg/${iconName}_off.svg", width: 22,)
+        ),
+        label: label,
+    );
+  }
+  
+  Widget _bottomNavigationBarWidget() {
+      return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          print(index);
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        currentIndex: _currentPageIndex,
+        selectedFontSize: 12,
+        selectedItemColor: Colors.black,  // label 색 추가
+        items: [
+          _bottomNavigationBarItem("home", "홈"),
+          _bottomNavigationBarItem("notes", "동네생활"),
+          _bottomNavigationBarItem("location", "내 근처"),
+          _bottomNavigationBarItem("chat", "채팅"),
+          _bottomNavigationBarItem("user", "나의 당근"),
+        ],
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appbarWidget(),
       body: _bodyWidget(),
-      // bottomNavigationBar: Container(),
+      bottomNavigationBar: _bottomNavigationBarWidget(),
     );
   }
 }
